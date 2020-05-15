6929713f294bdbeee6ba58551056583f9e1c1177
Miguel235711 <a01423629@itesm.mx>
2020-05-15 12:10:19 -0500

    complete reestructuration to pyqt

--- main.py
+++ main.py
@@ -1,84 +1,12 @@
-import cv2 as cv
-import numpy as np
+from PyQt5.QtWidgets import QApplication
 import file
-import window
+import mainWindow
 def readCV():
     cvFile=file.File('eeg_flex_007_Desconocido.csv')
     cvFile.readAndSaveData()
-def sampleDrawingsInWindows():
-    W = 400
-    windowInstance=window.Window('classed based window',(W, W, 3))
-    windowInstance2=window.Window('classed based window2',(W, W, 3))
-    windowInstance.drawEllipse(
-        (W // 2, W // 2),
-        (W // 4, W // 16),
-        90,
-        (255, 0, 0),
-        2,
-        8
-    )
-    windowInstance.drawEllipse(
-        (W // 2, W // 2),
-        (W // 4, W // 16),
-        45,
-        (255, 0, 0),
-        2,
-        8
-    )
-    windowInstance.drawEllipse(
-        (W // 2, W // 2),
-        (W // 4, W // 16),
-        -45,
-        (255, 0, 0),
-        2,
-        8
-    )
-    windowInstance.drawEllipse(
-        (W // 2, W // 2),
-        (W // 4, W // 16),
-        0,
-        (255, 0, 0),
-        2,
-        8
-    )
-    windowInstance.drawCircle(
-        (W // 2, W // 2),
-        W // 32,
-        (0, 0, 255),
-        -1,
-        8
-    )
-    windowInstance.drawRectangle(
-        (0, 7 * W // 8),
-        (W, W),
-        (0, 255, 255),
-        -1,
-        8
-    )
-    windowInstance.drawLine((0, 15 * W // 16), (W, 15 * W // 16),(0,0,0),2,8)
-    windowInstance.drawLine((W // 4, 7 * W // 8), (W // 4, W),(0,0,0),2,8)
-    windowInstance.drawLine((W // 2, 7 * W // 8), (W // 2, W),(0,0,0),2,8)
-    windowInstance.drawLine((3 * W // 4, 7 * W // 8), (3 * W // 4, W),(0,0,0),2,8)
-    windowInstance2.drawConvexPolygon(
-        [
-         [W / 4, 7 * W / 8], [3 * W / 4, 7 * W / 8],
-         [3 * W / 4, 13 * W / 16], [11 * W / 16, 13 * W / 16],
-         [19 * W / 32, 3 * W / 8], [3 * W / 4, 3 * W / 8],
-         [3 * W / 4, W / 8], [26 * W / 40, W / 8],
-         [26 * W / 40, W / 4], [22 * W / 40, W / 4],
-         [22 * W / 40, W / 8], [18 * W / 40, W / 8],
-         [18 * W / 40, W / 4], [14 * W / 40, W / 4],
-         [14 * W / 40, W / 8], [W / 4, W / 8],
-         [W / 4, 3 * W / 8], [13 * W / 32, 3 * W / 8],
-         [5 * W / 16, 13 * W / 16], [W / 4, 13 * W / 16]
-        ],
-        8
-    )
-    windowInstance.showWindow()
-    windowInstance2.showWindow()
-    cv.waitKey(0)
-    cv.destroyAllWindows()
 def main():
     readCV()
-    sampleDrawingsInWindows() 
+    app=QApplication([])
+    mainWindowInstance=mainWindow.MainWindow('ABMODEL','images/cheese.jpg')
+    app.exec_()
 main()
---
+++ mainWindow.py
@@ -0,0 +1,35 @@
+from PyQt5.QtWidgets import QWidget,QMainWindow, QPushButton, QVBoxLayout,QMessageBox,QToolTip
+from PyQt5.QtGui import QIcon,QFont
+from pyqtgraph import PlotWidget, plot
+
+class MainWindow(QMainWindow):
+    def __init__(self,title,iconPath):
+        super(QWidget,self).__init__()
+        self.title=title
+        self.iconPath=iconPath
+        self.initUI()
+    def initGraph(self):
+        self.graphWidget=PlotWidget()
+        self.setCentralWidget(self.graphWidget)
+        hour = [1,2,3,4,5,6,7,8,9,10]
+        temperature = [30,32,34,32,33,31,29,32,35,45]
+        self.graphWidget.plot(hour,temperature)
+    def initUI(self):
+        self.initGraph()
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setToolTip('This is a <b>QWidget</b> widget')
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+        self.setWindowTitle(self.title)
+        self.setWindowIcon(QIcon(self.iconPath))
+        self.showMaximized()
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message',
+            "Are you sure to quit?", QMessageBox.Yes | 
+            QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
