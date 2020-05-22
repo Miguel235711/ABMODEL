9f88bcca5c9eed7a0268371667a1d29d4b248864
Miguel235711 <a01423629@itesm.mx>
2020-05-22 15:04:02 -0500

    folder structure improved

--- Modules/Grapher/grapherMain.py
+++ Modules/Grapher/grapherMain.py
@@ -1,14 +1,16 @@
+import sys
 from PyQt5.QtWidgets import QApplication
+sys.path.append('../Data/Reader')
 import file
 import mainWindow
 def readCV():
-    cvFile=file.File('eeg_flex_007_Desconocido.csv')
+    cvFile=file.File('../../Data/Graph/eeg_flex_007_Desconocido.csv')
     cvFile.readAndSaveData()
     return cvFile
 def main():
     cvFile=readCV()
     app=QApplication([])
-    mainWindowInstance=mainWindow.MainWindow('ABMODEL','images/cheese.jpg')
+    mainWindowInstance=mainWindow.MainWindow('ABMODEL','/Public/Images/cheese.jpg')
     #dummy=[cvFile.getAverageOfWavesAndTime(0),cvFile.getAverageOfWavesAndTime(1),cvFile.getAverageOfWavesAndTime(2),cvFile.getAverageOfWavesAndTime(3)]
     #data
     data=cvFile.getAllData()
