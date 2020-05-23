bd9610cb7d54942802f6617cb77f0f8e14bdd091
Miguel235711 <a01423629@itesm.mx>
2020-05-22 19:22:20 -0500

    separating all 4 types of waves with layout

--- Modules/Grapher/main.py
+++ Modules/Grapher/main.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 sys.path.append('../Data/Reader')
 import file
-import mainWindow
+import window
 def readCV():
     cvFile=file.File('../../Data/Graph/eeg_flex_007_Desconocido.csv')
     cvFile.readAndSaveData()
@@ -10,11 +10,11 @@ def readCV():
 def main():
     cvFile=readCV()
     app=QApplication([])
-    mainWindowInstance=mainWindow.MainWindow('ABMODEL','../../Public/Images/cheese.jpg')
+    mainWindowInstance=window.Window('ABMODEL','../../Public/Images/cheese.jpg')
     #dummy=[cvFile.getAverageOfWavesAndTime(0),cvFile.getAverageOfWavesAndTime(1),cvFile.getAverageOfWavesAndTime(2),cvFile.getAverageOfWavesAndTime(3)]
     #data
     data=cvFile.getAllData()
-    print(data)
-    mainWindowInstance.initGraph(data)
+    #print(data)
+    mainWindowInstance.initGraphs(data)
     app.exec_()
 main()
