bce1ba0f8a419743c094af034532f5ab41c7def6
Nemesis897GT <nemesisdarkness897gt@gmail.com>
2020-05-26 15:27:10 -0500

    HelloWorld: AuthenticationMain added Users encriptation added Passwords encriptation added

---
+++ Modules/Authentication/users.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+def UsersEncriptation(_users, _password):
+    match = 0
+    usuarioBase = _users
+    encriptation = 32
+    while(not match >= 2 or encriptation > 256):
+        _users = usuarioBase
+        temp_number = 0
+
+        for x in _password:
+            temp_number += ord(x)
+
+        for x in _users:
+            _users = _users.replace(x, chr((temp_number + ord(x)) % encriptation), 1)
+            #print ((temp_number + ord(x)) % encriptation)
+
+
+        for x in _users:
+            if(x == "$" or x == "/" or x == "!" or x == "-" or x == "K" or x == " " or x == "7"):
+                match+= 1
+        encriptation += 16
+    print _users
+    print encriptation
